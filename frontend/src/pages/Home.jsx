1b2b956c44b12d0274097868f31139da52fff7f7
latentlearner <latentlearner@users.noreply.github.com>
2026-03-04 23:37:22 +0400

    feat: enhance MovieCard component and implement movie fetching functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,48 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
+import "../css/Home.css";
 
 function Home() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const movies = [
-    { id: 1, title: "The Shawshank Redemption", release_date: "1994-09-23" },
-    { id: 2, title: "The Godfather", release_date: "1972-03-24" },
-    { id: 3, title: "The Dark Knight", release_date: "2008-07-18" },
-    { id: 4, title: "Pulp Fiction", release_date: "1994-10-14" },
-    { id: 5, title: "Fight Club", release_date: "1999-10-15" },
-    { id: 6, title: "Inception", release_date: "2010-07-16" },
-    { id: 7, title: "The Matrix", release_date: "1999-03-31" },
-    { id: 8, title: "Forrest Gump", release_date: "1994-07-06" },
-  ];
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  const onSearchSubmit = (event) => {
+    loadPopularMovies();
+  }, []);
+
+  const onSearchSubmit = async (event) => {
     event.preventDefault();
-    alert(`Search ${searchTerm} submitted`);
+    if (!searchTerm.trim()) return;
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const searchResults = await searchMovies(searchTerm);
+      setMovies(searchResults);
+      setError(null);
+      // setSearchTerm("");
+    } catch (err) {
+      console.log(err);
+      setError("Failed to search movies...");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -35,11 +60,17 @@ function Home() {
         </button>
       </form>
 
-      <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </div>
+      {error && <div className="error">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_BASE = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(
+    `${API_BASE}/movie/popular?api_key=${API_KEY}`
+  );
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${API_BASE}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query) }`
+    );
+    const data = await response.json();
+    return data.results;
+};
